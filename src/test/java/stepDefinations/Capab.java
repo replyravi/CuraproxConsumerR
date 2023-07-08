9242a21e49d05091a148b4603c703c8c025bdbd4
ravipifinancial <ravi@pi.financial>
2023-07-08 19:56:21 +0700

    Initial commit

--- src/test/java/stepDefinations/Capab.java
+++ src/test/java/stepDefinations/Capab.java
@@ -4,6 +4,7 @@ import java.net.MalformedURLException;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+// main branch commit
 
 public class Capab extends Base {
 
